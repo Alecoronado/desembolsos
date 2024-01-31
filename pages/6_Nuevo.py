0685007b8651730586e81c9f92aff07c8512ec08
Alecoronado <153213444+Alecoronado@users.noreply.github.com>
2024-01-31 19:47:05 +0000

    qwfr

--- pages/6_Nuevo.py
+++ pages/6_Nuevo.py
@@ -10,8 +10,6 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error, r2_score
-import skopt
-from skopt import BayesSearchCV
 from sklearn.ensemble import GradientBoostingRegressor
 
 # Función para cargar datos
